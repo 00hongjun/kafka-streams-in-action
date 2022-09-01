d31625eb349b31d682f68e733b4340cd83139f2f
00hongjun <00hongjun@gmail.com>
2022-09-02 00:15:53 +0900

    style: chapter3 내용 정리

--- src/main/java/bbejeck/chapter_3/ZMartKafkaStreamsAdvancedReqsApp.java
+++ src/main/java/bbejeck/chapter_3/ZMartKafkaStreamsAdvancedReqsApp.java
@@ -50,19 +50,19 @@ public class ZMartKafkaStreamsAdvancedReqsApp {
 
         StreamsConfig streamsConfig = new StreamsConfig(getProperties());
 
-        Serde<Purchase> purchaseSerde = StreamsSerdes.PurchaseSerde();
+        Serde<Purchase> purchaseSerde = StreamsSerdes.PurchaseSerde(); /** 저자는 [StreamsSerdes] 유틸을 만들고 그 안에 Serd들을 선언했다. */
         Serde<PurchasePattern> purchasePatternSerde = StreamsSerdes.PurchasePatternSerde();
         Serde<RewardAccumulator> rewardAccumulatorSerde = StreamsSerdes.RewardAccumulatorSerde();
         Serde<String> stringSerde = Serdes.String();
 
-        StreamsBuilder builder = new StreamsBuilder();
+        StreamsBuilder builder = new StreamsBuilder(); /** 이 builder를 이용해 [KafkaStreams]을 생성한다 */
 
 
         // previous requirements
-        KStream<String,Purchase> purchaseKStream = builder.stream( "transactions", Consumed.with(stringSerde, purchaseSerde))
-                .mapValues(p -> Purchase.builder(p).maskCreditCard().build());
+        KStream<String,Purchase> purchaseKStream = builder.stream( "transactions", Consumed.with(stringSerde, purchaseSerde)) /** [purchaseKStream]이 아래에서 사용할 source 이다 */
+                .mapValues(p -> Purchase.builder(p).maskCreditCard().build()); // value를 읽고 [Purchase]를 생성한다
 
-        KStream<String, PurchasePattern> patternKStream = purchaseKStream.mapValues(purchase -> PurchasePattern.builder(purchase).build());
+        KStream<String, PurchasePattern> patternKStream = purchaseKStream.mapValues(purchase -> PurchasePattern.builder(purchase).build()); /** 위에서 생성한 [purchaseKStream]를 이용하여 [PurchasePattern]을 생성 */
 
         patternKStream.print( Printed.<String, PurchasePattern>toSysOut().withLabel("patterns"));
         patternKStream.to("patterns", Produced.with(stringSerde,purchasePatternSerde));
@@ -70,34 +70,39 @@ public class ZMartKafkaStreamsAdvancedReqsApp {
 
         KStream<String, RewardAccumulator> rewardsKStream = purchaseKStream.mapValues(purchase -> RewardAccumulator.builder(purchase).build());
 
-        rewardsKStream.print(Printed.<String, RewardAccumulator>toSysOut().withLabel("rewards"));
-        rewardsKStream.to("rewards", Produced.with(stringSerde,rewardAccumulatorSerde));
+        rewardsKStream.print(Printed.<String, RewardAccumulator>toSysOut().withLabel("rewards")); /** [KStream.print] -> stout 출력*/
+        rewardsKStream.to("rewards", Produced.with(stringSerde,rewardAccumulatorSerde)); /** [rewards] 토픽에 쓴다 */
 
 
 
            // selecting a key for storage and filtering out low dollar purchases
 
 
-        KeyValueMapper<String, Purchase, Long> purchaseDateAsKey = (key, purchase) -> purchase.getPurchaseDate().getTime();
+        KeyValueMapper<String, Purchase, Long> purchaseDateAsKey = (key, purchase) -> purchase.getPurchaseDate().getTime(); /** Long 타입 구매 날짜를 key로 변환하는 KeyValueMapper */
 
-        KStream<Long, Purchase> filteredKStream = purchaseKStream.filter((key, purchase) -> purchase.getPrice() > 5.00).selectKey(purchaseDateAsKey);
+        KStream<Long, Purchase> filteredKStream = purchaseKStream.filter((key, purchase) -> purchase.getPrice() > 5.00).selectKey(purchaseDateAsKey); /** 5달러 미만의 구매 필터링, 위에서 생성한 KeyValueMapper를 이용한다. -> 새로운 key 생성 */
 
         filteredKStream.print(Printed.<Long, Purchase>toSysOut().withLabel("purchases"));
-        filteredKStream.to("purchases", Produced.with(Serdes.Long(),purchaseSerde));
+        filteredKStream.to("purchases", Produced.with(Serdes.Long(),purchaseSerde)); /** key type이 변경 됐으므로 serd도 변경 된다 */
 
 
 
          // branching stream for separating out purchases in new departments to their own topics
 
-        Predicate<String, Purchase> isCoffee = (key, purchase) -> purchase.getDepartment().equalsIgnoreCase("coffee");
+        Predicate<String, Purchase> isCoffee = (key, purchase) -> purchase.getDepartment().equalsIgnoreCase("coffee"); /** 이 [Predicate]를 이용해 스트림을 분리할 수 있다. */
         Predicate<String, Purchase> isElectronics = (key, purchase) -> purchase.getDepartment().equalsIgnoreCase("electronics");
 
-        int coffee = 0;
+        int coffee = 0;  /** [KStream.branch] 의 예상 index값이다. 아래 코드에서 [.branch(isCoffee, isElectronics)]으로 0, 1 순서로 처리했다 */
         int electronics = 1;
 
         KStream<String, Purchase>[] kstreamByDept = purchaseKStream.branch(isCoffee, isElectronics);
+        /** [KStream.branch]로 스트림을 분리 한다.
+         * 커피, 전자제품으로 분리
+         * 배열은 인자로 받은 predicate의 순으로 배정되며, filter에 걸리지 않은 레코드는 삭제된다.
+         */
 
-        kstreamByDept[coffee].to( "coffee", Produced.with(stringSerde, purchaseSerde));
+
+        kstreamByDept[coffee].to( "coffee", Produced.with(stringSerde, purchaseSerde)); /** coffee 토픽에 쓰기 */
         kstreamByDept[coffee].print(Printed.<String, Purchase>toSysOut().withLabel( "coffee"));
 
         kstreamByDept[electronics].to("electronics", Produced.with(stringSerde, purchaseSerde));
@@ -108,7 +113,7 @@ public class ZMartKafkaStreamsAdvancedReqsApp {
 
          // security Requirements to record transactions for certain employee
         ForeachAction<String, Purchase> purchaseForeachAction = (key, purchase) ->
-                SecurityDBService.saveRecord(purchase.getPurchaseDate(), purchase.getEmployeeId(), purchase.getItemPurchased());
+                SecurityDBService.saveRecord(purchase.getPurchaseDate(), purchase.getEmployeeId(), purchase.getItemPurchased()); /** [ForeachAction<K, V>]을 정의 한다 */
 
         
         purchaseKStream.filter((key, purchase) -> purchase.getEmployeeId().equals("000000")).foreach(purchaseForeachAction);
